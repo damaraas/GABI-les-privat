c701ea2582562d98328f785b0dea717355ec59f0
damaraas <damarasalsabila@gmail.com>
2025-10-06 13:35:05 +0700

    refactor: posisi floating wa

--- resources/js/Components/FloatingWA.vue
+++ resources/js/Components/FloatingWA.vue
@@ -9,7 +9,7 @@ console.log(waLink);
 </script>
 
 <template>
-    <a :href="waLink" target="_blank" rel="noopener noreferrer" class="fixed bottom-8 right-12 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 flex items-center justify-center z-50">
+    <a :href="waLink" target="_blank" rel="noopener noreferrer" class="fixed bottom-8 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 flex items-center justify-center z-50">
         <SiWhatsapp class="w-8 h-8" />
     </a>
 </template>
