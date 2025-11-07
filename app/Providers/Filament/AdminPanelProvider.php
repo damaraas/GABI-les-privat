54f89a07662a628aff6094f84959df1dc91496f3
damaraas <damarasalsabila@gmail.com>
2025-11-07 16:44:18 +0700

    hapus  profile()

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->profile()
             ->brandName('Gabi Les Privat')
             ->favicon('favicon.webp')
             ->colors([
